8e48a1ac6e29d7ecd5a04f2b00c7bd1b09e86f29
MohAzril <azril@alphatech.id>
2019-03-05 14:33:52 +0700

    tambah database karyawan

---
+++ env_scrum/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alphatech/kumpulan/SCRUM/SCRUM/env_scrum/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- simulasi_scrum/app_simulasi/models.py
+++ simulasi_scrum/app_simulasi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Karyawan(models.Model):
+    nama = models.CharField(max_length=255)
+    telepon = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nama
